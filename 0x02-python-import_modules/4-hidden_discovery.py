2001a10bf972fcdf926af5da6cf59e54478c554b
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-02 13:11:08 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from hidden_4 import *
     all_names = dir()
-    for i in range(0, len(all_names))
+    for i in range(0, len(all_names)):
     if all_names[i][:2] != "__":
         print("{:s}".format(all_names[i]))
