32ba81c0a52815d2cc910ace1109cc3fae62436d
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-05 02:36:56 -0800

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,4 @@ def search_replace(my_list, search, replace):
     for index, items in enumerate(new_list):
         if items == search:
             new_list[index] = replace
-        return new_list
+        return (new_list)
