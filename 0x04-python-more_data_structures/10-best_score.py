2773e249edd21847be2768d4d30ef036d84cbb6f
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-05 07:36:47 -0800

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    best_score = float('-inf')
+    best_key = None
+    for key, value in a_dictionary.items():
+        if value > best_score:
+            best_score = value
+            best_key = key
+    return (best_key)
