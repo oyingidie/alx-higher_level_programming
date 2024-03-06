06a64703c88e663fe14f57afaf51c6cea7512b10
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-06 01:35:59 -0800

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,26 @@
-#!/usr/bn/python3
-
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    if type(roman_string) != str or roman_string == None:
-        return (0)
-    for i, char in enumerate(roman_string):
-        temp = roman_dic[char]
-        try:
-            if temp < roman_dic[roman_string[i + 1]]:
-                temp = temp * -1
-            except IndexError:
-                pass
-            num = num + temp
-        return (num)
+    if roman_string == None or type(roman_string) != str:
+        r_num = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000
+                }
+        result = 0
+        i = 0
+        while i < len(roman_string):
+            c_char = roman_string[i]
+            c_value = r_num.get(c_char)
+        if not value:
+            return (0)
+        if i + 1 < len(roman_string) and c_value < r_num.get(roman_string[i + 1]):
+            result += r_num[roman_string[i + 1]] - c_value
+            i += 2
+        else:
+            result += c_value
+            i += 1
+    return (result)
