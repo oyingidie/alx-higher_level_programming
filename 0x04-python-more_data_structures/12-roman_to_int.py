539b0a609e27cd58aded70923fe75202f19ab2c7
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-06 00:56:09 -0800

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,6 @@
 #!/usr/bn/python3
 def roman_to_int(roman_string):
-    roman_dic = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-            }
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     if type(roman_string) != str or roman_string == None:
         return (0)
