b5ced3261c58f78a8dd3b1f04443314b0a1181bf
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-06 03:09:57 -0800

    Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,7 @@ def weight_average(my_list=[]):
         return (0)
     scores = 0
     weights = 0
-    for tup in my_list:
+    for int_tuple in my_list:
         scores += int_tuple[0] * int_tuple[1]
         weights += int_tuple[1]
     return (scores / weights)
