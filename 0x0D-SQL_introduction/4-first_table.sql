5899b943b609fc4adfd7500a373144c29093613c
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-17 11:19:47 -0700

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- create a table called 'first_table' in current database in MySQL server
+-- no SELECT or SHOW statements allowed
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
