e8addad99c2937f69e91e4f56e802790a7d69064
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-18 01:31:50 -0700

    Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- import table dump in a database in my MySQL server
 -- display temperature of the top three cities during July and August
-SELECT city, AVG(value) AS average_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY average_temp DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
