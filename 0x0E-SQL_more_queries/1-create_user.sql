f9a7bfaf31df435c9107596fe9d8751325e36302
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-18 03:42:53 -0700

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create a MySQL server user
+-- user should have all privileges on the server
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
