f3afddb9c3f52ea90a1035658a91738924a79187
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-17 21:19:29 -0700

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- convert database in my MySQL server to UTF8
+-- also convert a table in the database & a field in the table
+USE hbtn_0c_0
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicide_ci;
