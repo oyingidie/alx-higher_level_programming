df94c94dd98ffb65dac73b86006771f817fe2df5
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-18 21:43:23 -0700

    Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- list all shows contained in the database 'hbtn_0d_tvshows'
+-- result must be in ascending order of 'tv_shows.title' & 'tv_show_genres.genre_id'
+-- display NULL if a show has no genre
+-- only one SELECT statement can be used
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
