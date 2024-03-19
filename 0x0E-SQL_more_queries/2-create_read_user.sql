79abdf2de068c61443cbedbf0af13b6918550361
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-18 20:27:29 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- create a new database and user
 -- user should have only SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2_pwd@localhost IDENTIFIED BY "user_0d_2_pwd";
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd";
 GRANT SELECT ON hbtn_0d_2. * TO user_0d_2@localhost;
