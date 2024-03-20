98a5f84cf87f77526344fb55a1585d38486e3dd8
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-20 04:26:59 -0700

    Rotten tomatoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- list all shows from 'hbtn_0d_tvshows_rate' by their rating
+-- records must be sorted in descending order of the rating
+-- only one SELECT statement can be used
+SELECT title, SUM(tv_show_ratings.rate) AS "rating"
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
