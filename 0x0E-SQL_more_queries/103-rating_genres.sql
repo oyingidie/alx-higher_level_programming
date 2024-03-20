fb2bb62e63cd1f72ae04d80df2b402567189922c
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-20 04:38:23 -0700

    Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- list all genres in the database 'hbtn_0d_tvshows_rate' by their rating
+-- result must be in descending order of their rating
+-- only one SELECT statement can be used
+SELECT name, SUM(tv_show_ratings.rate) AS "rating"
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
