2a92c28d1e77f06cb4f33c39551c8f92e3cce3c5
oyingidie <oyinkansoladebayo@gmail.com>
2024-04-08 23:35:14 -0700

    Print and count integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for items in range(x):
         try:
             print("{:d}".format(my_list[items]), end='')
-            num++
+            num += 1
         except (ValueError, TypeError, IndexError):
             pass
         print()
