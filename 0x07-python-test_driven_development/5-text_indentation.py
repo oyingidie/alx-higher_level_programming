982ff41c76c68d3f25a2d977d0c104db015cd525
oyingidie <oyinkansoladebayo@gmail.com>
2024-04-24 11:33:47 -0700

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""a function that prints a text with
+two new lines after selected characters
+"""
+
+
+def text_indentation(text):
+    """splits a text into lines
+    Args:
+        text: input string
+    Raises:
+        TypeError: if text is not a string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    a = 0
+    while a < len(text):
+        if flag == 0:
+            if text[a] == ' ':
+                a += 1
+                continue
+            else:
+                flag = 1
+        elif flag == 1:
+            if text[a] == '?' or text[a] == '.' or text[a] == ':':
+                print(text[a])
+                print()
+                flag = 0
+            else:
+                if text[a + 1] == '\n':
+                    a += 1
+                    continue
+                print(text[a], end="")
+            a += 1
