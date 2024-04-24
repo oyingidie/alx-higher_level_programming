cd62ce135f899af8974e2e1c8682fa9b1b203c02
oyingidie <oyinkansoladebayo@gmail.com>
2024-04-24 10:56:59 -0700

    Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a function that prints square with '#' character"""
+
+
+def print_square(size):
+    """prints a square of given size
+    Args:
+        size (int): size of square to be printed
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than zero
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    for x in range(size):
+        print('#' * size)
