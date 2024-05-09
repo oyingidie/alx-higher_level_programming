5c6ee795247d533c8587560643b0603b8812a7d2
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-08 17:46:53 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class with a public instance method"""
+
+
+class BaseGeometry:
+    """defines a base geometry"""
+
+    def area(self):
+        """unimplemented method"""
+        raise Exception("area() is not implemented")
