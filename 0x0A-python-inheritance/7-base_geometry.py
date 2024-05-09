7afdfa0c388a149bce883863349c0fb4d9984e56
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-08 18:16:05 -0700

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class with public instance methods"""
+
+
+class BaseGeometry:
+    """defines a base geometry"""
+
+    def area(self):
+        """unimplemented method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, naame, value):
+        """validates value as a positive integer"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
