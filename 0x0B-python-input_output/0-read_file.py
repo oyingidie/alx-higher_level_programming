a316909da66e8dba2466ca07797a69d3872505f6
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-13 01:39:13 -0700

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ and prints to stdout
 
 
 def read_file(filename=""):
-    """read text file"""
+    """read text file
+    Args:
+        filename (str): name of th file  to read
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
