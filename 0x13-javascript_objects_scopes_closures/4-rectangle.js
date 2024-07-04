1378696685dd8d6e5610c759d692d30fe5c4eff3
root <oyingidie>
2024-07-04 03:05:23 -0700

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,9 +9,15 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+    do {
+      let row = '';
+      for (let i = 0; i < this.width; i++) {
+        row += 'X';
+      }
+
+      console.log(row);
+      this.height--;
+    } while (this.height > 0);
   }
 
   rotate () {
