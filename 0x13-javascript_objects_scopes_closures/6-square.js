a58fc9f49102b686c7fa0edd7a17c1c68ade39ce
root <oyingidie>
2024-07-15 09:09:43 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const squareZero = require('./5-square.js');
+
+module.exports = class Square extends squareZero {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+};
