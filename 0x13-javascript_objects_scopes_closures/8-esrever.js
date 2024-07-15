a830ea9e0712ce551571a9071deefbf95c4166e8
root <oyingidie>
2024-07-15 14:04:17 -0700

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const reversedList = [];
 
-  for (let i = list.length; i >= 1; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
 
